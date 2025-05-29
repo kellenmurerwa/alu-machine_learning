085018cc495f1c71a50609cd0f4e348e3a1ac63b
Kellen Murerwa <145454946+kellenmurerwa@users.noreply.github.com>
2025-05-30 00:01:38 +0200

    Determinant of a matrix

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+A function def determinant(matrix): 
+that calculates the determinant of a matrix
+"""
+
+
+def determinant(matrix):
+    """
+    Calculating the determinant of a matrix and returning it.
+    """
+    if matrix == [[]]:
+        return 1
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if not all(len(matrix) == len(row) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+
+    n = len(matrix)
+    det = 0
+    for j in range(n):
+        sign = (-1)**j
+        sub_matrix = [row[:j] + row[j+1:] for row in matrix[1:]]
+        det += sign * matrix[0][j] * determinant(sub_matrix)
+    return det
