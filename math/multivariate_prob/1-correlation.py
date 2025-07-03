7be87701f208354ce8efaab981a216ee04d80617
Kellen Murerwa <145454946+kellenmurerwa@users.noreply.github.com>
2025-07-03 15:48:13 +0200

    correlation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -15,10 +15,8 @@ def correlation(C):
         raise TypeError("C must be a numpy.ndarray")
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    
     d = C.shape[0]
     std_dev = np.sqrt(np.diag(C))
     std_dev_matrix = np.outer(std_dev, std_dev)
     corr = C / std_dev_matrix
-    
     return corr
